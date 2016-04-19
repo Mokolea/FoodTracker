b8cfbfa55da6a9d7f2e97c344793aef83edd6da2
Mario Ban <mario.ban@bluewin.ch>
2016-04-19 19:35:03 +0200

    Photo (default) added

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, /*protocol*/ UITextFieldDelegate {
   
   // MARK: Properties
   @IBOutlet weak var nameTextField: UITextField!
