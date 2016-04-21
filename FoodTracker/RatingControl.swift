73fecf726c92f8fad253e9f144cb892cb9264c1b
Mario Ban <mario.ban@bluewin.ch>
2016-04-21 19:34:48 +0200

    Create a custom view

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,27 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Mario Ban on 21.04.16.
+//  Copyright © 2016 Mario Ban. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+  /*
+  // Only override drawRect: if you perform custom drawing.
+  // An empty implementation adversely affects performance during animation.
+  override func drawRect(rect: CGRect) {
+    // Drawing code
+  }
+  */
+
+  // MARK: Initialization
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+
+  }
+
+}
