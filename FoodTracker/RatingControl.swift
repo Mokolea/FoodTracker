d9ce251247add92ccfeb808b945c432f0ae39deb
Mario Ban <mario.ban@bluewin.ch>
2016-04-23 16:18:41 +0200

    Add a button to the view

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -22,6 +22,20 @@ class RatingControl: UIView {
   required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
 
+    let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+    button.backgroundColor = UIColor.redColor()
+
+    button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), forControlEvents: .TouchDown)
+
+    addSubview(button)
+  }
+  override func intrinsicContentSize() -> CGSize {
+    return CGSize(width: 240, height: 44)
+  }
+
+  // MARK: Button Action
+  func ratingButtonTapped(button: UIButton) {
+    print("Button pressed 👍")
   }
 
 }
