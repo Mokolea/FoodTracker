97de30fae84f615f5cc0864e834800ad4042f955
Mario Ban <mario.ban@bluewin.ch>
2016-04-23 16:42:40 +0200

    Add 5 buttons to the view

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -18,16 +18,30 @@ class RatingControl: UIView {
   }
   */
 
+  // MARK: Properties
+  var rating = 0
+  var ratingButtons = [UIButton]()
+
   // MARK: Initialization
   required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
 
-    let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
-    button.backgroundColor = UIColor.redColor()
+    for _ in 0..<5 {
+      let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
+      button.backgroundColor = UIColor.redColor()
+      button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), forControlEvents: .TouchDown)
+      ratingButtons += [button]
+      addSubview(button)
+    }
+  }
+  override func layoutSubviews() {
+    var buttonFrame = CGRect(x: 0, y: 0, width: 44, height: 44)
 
-    button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), forControlEvents: .TouchDown)
-
-    addSubview(button)
+    // Offset each button's origin by the length of the button plus spacing.
+    for (index, button) in ratingButtons.enumerate() {
+      buttonFrame.origin.x = CGFloat(index * (44 + 5))
+      button.frame = buttonFrame
+    }
   }
   override func intrinsicContentSize() -> CGSize {
     return CGSize(width: 240, height: 44)
