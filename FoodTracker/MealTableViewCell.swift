8c066180356b0ce69ee7a165796147ba75464e2b
Mario Ban <mario.ban@bluewin.ch>
2016-04-24 18:38:53 +0200

    Connect the table cell UI to code

--- FoodTracker/MealTableViewCell.swift
+++ FoodTracker/MealTableViewCell.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class MealTableViewCell: UITableViewCell {
 
+  // MARK: Properties
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var photoImageView: UIImageView!
+  @IBOutlet weak var ratingControl: RatingControl!
+
   override func awakeFromNib() {
     super.awakeFromNib()
     // Initialization code
