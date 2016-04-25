57c168c3b5372b6c4bae368b2776684bf4fd2525
Mario Ban <mario.ban@bluewin.ch>
2016-04-25 09:10:35 +0200

    Display the data

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -46,26 +46,28 @@ class MealTableViewController: UITableViewController {
   }
 
   // MARK: - Table view data source
-
   override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-    // #warning Incomplete implementation, return the number of sections
-    return 0
+    return 1
   }
 
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    // #warning Incomplete implementation, return the number of rows
-    return 0
+    return meals.count
   }
 
-  /*
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+    // Table view cells are reused and should be dequeued using a cell identifier.
+    let cellIdentifier = "MealTableViewCell"
+    let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! MealTableViewCell
 
-    // Configure the cell...
+    // Fetches the appropriate meal for the data source layout.
+    let meal = meals[indexPath.row]
+
+    cell.nameLabel.text = meal.name
+    cell.photoImageView.image = meal.photo
+    cell.ratingControl.rating = meal.rating
 
     return cell
   }
-  */
 
   /*
   // Override to support conditional editing of the table view.
