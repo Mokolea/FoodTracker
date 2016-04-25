42e230afe46c2ae307941fa00ee4c32815855274
Mario Ban <mario.ban@bluewin.ch>
2016-04-25 18:16:19 +0200

    Cancel an edit to an existing meal

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -96,7 +96,15 @@ class MealViewController: UIViewController, /* protocols */ UITextFieldDelegate,
   // MARK: Navigation
 
   @IBAction func cancel(sender: UIBarButtonItem) {
-    dismissViewControllerAnimated(true, completion: nil)
+    // Depending on style of presentation (modal or push presentation), this view controller needs to be dismissed in two different ways.
+    let isPresentingInAddMealMode = presentingViewController is UINavigationController
+
+    if isPresentingInAddMealMode {
+      dismissViewControllerAnimated(true, completion: nil)
+    }
+    else {
+      navigationController!.popViewControllerAnimated(true)
+    }
   }
 
   // This method lets you configure a view controller before it's presented.
