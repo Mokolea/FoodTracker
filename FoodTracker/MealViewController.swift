a2b35eef0acfd6f80513db2831c3130faeeea9e9
Mario Ban <mario.ban@bluewin.ch>
2016-04-25 13:01:35 +0200

    Connect save button

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -15,6 +15,13 @@ class MealViewController: UIViewController, /* protocols */ UITextFieldDelegate,
   //@IBOutlet weak var mealNameLabel: UILabel!
   @IBOutlet weak var photoImageView: UIImageView!
   @IBOutlet weak var ratingControl: RatingControl!
+  @IBOutlet weak var saveButton: UIBarButtonItem!
+
+  /*
+   This value is either passed by `MealTableViewController` in `prepareForSegue(_:sender:)`
+   or constructed as part of adding a new meal.
+   */
+  var meal: Meal?
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -56,6 +63,20 @@ class MealViewController: UIViewController, /* protocols */ UITextFieldDelegate,
 
     // Dismiss the picker.
     dismissViewControllerAnimated(true, completion: nil)
+  }
+
+  // MARK: Navigation
+
+  // This method lets you configure a view controller before it's presented.
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if saveButton === sender {
+      let name = nameTextField.text ?? ""
+      let photo = photoImageView.image
+      let rating = ratingControl.rating
+
+      // Set the meal to be passed to MealTableViewController after the unwind segue.
+      meal = Meal(name: name, photo: photo, rating: rating)
+    }
   }
 
   // MARK: Actions
