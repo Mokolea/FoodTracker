ddf8a92ca50ab9b3f4029de559af6ad01973678b
Mario Ban <mario.ban@bluewin.ch>
2016-04-26 14:56:04 +0200

    Make the meal class data persistent

--- FoodTracker/Meal.swift
+++ FoodTracker/Meal.swift
@@ -9,13 +9,26 @@
 //import Foundation
 import UIKit
 
-class Meal {
+class Meal: NSObject, /* protocol */ NSCoding {
 
   // MARK: Properties
 
   var name: String
   var photo: UIImage?
   var rating: Int
+
+  // MARK: Archiving Paths
+
+  static let DocumentsDirectory = NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+  static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent("meals")
+
+  // MARK: Types
+
+  struct PropertyKey {
+    static let nameKey = "name"
+    static let photoKey = "photo"
+    static let ratingKey = "rating"
+  }
 
   // MARK: Initialization
   
@@ -25,10 +38,30 @@ class Meal {
     self.photo = photo
     self.rating = rating
 
+    super.init() // Call superclass initializer
+
     // Initialization should fail if there is no name or if the rating is out of range.
     if name.isEmpty || rating < 0 || rating > 5 {
       return nil
     }
+  }
+
+  // MARK: NSCoding
+
+  func encodeWithCoder(aCoder: NSCoder) {
+    aCoder.encodeObject(name, forKey: PropertyKey.nameKey)
+    aCoder.encodeObject(photo, forKey: PropertyKey.photoKey)
+    aCoder.encodeInteger(rating, forKey: PropertyKey.ratingKey)
+  }
+
+  required convenience init?(coder aDecoder: NSCoder) {
+    let name = aDecoder.decodeObjectForKey(PropertyKey.nameKey) as! String
+    // Because photo is an optional property of Meal, use conditional cast.
+    let photo = aDecoder.decodeObjectForKey(PropertyKey.photoKey) as? UIImage
+    let rating = aDecoder.decodeIntegerForKey(PropertyKey.ratingKey)
+
+    // Must call designated initializer.
+    self.init(name: name, photo: photo, rating: rating)
   }
 
 }
