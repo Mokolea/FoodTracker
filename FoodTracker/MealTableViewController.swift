0ec3576d4b2e322301d947ada8181c1d3682c323
Mario Ban <mario.ban@bluewin.ch>
2016-04-26 15:37:05 +0200

    Save and load the meal list

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -26,8 +26,15 @@ class MealTableViewController: UITableViewController {
     // Use the edit button item provided by the table view controller.
     navigationItem.leftBarButtonItem = editButtonItem()
 
-    // Load the sample data.
-    loadSampleMeals()
+    // Load any saved meals, otherwise load sample data.
+    if let savedMeals = loadMeals() {
+      meals += savedMeals
+      printMeals() // Debug
+    }
+    else {
+      // Load the sample data.
+      loadSampleMeals()
+    }
   }
 
   override func didReceiveMemoryWarning() {
@@ -95,11 +102,11 @@ class MealTableViewController: UITableViewController {
     if editingStyle == .Delete {
       // Delete the row from the data source
       meals.removeAtIndex(indexPath.row)
+      saveMeals()
       tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
     } else if editingStyle == .Insert {
       // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     }
-    printMeals() // Debug
   }
 
   // Override to support rearranging the table view.
@@ -154,8 +161,23 @@ class MealTableViewController: UITableViewController {
         meals.append(meal)
         tableView.insertRowsAtIndexPaths([newIndexPath], withRowAnimation: .Bottom)
       }
-      printMeals() // Debug
+      // Save the meals.
+      saveMeals()
     }
+  }
+
+  // MARK: NSCoding
+
+  func saveMeals() {
+    let isSuccessfulSave = NSKeyedArchiver.archiveRootObject(meals, toFile: Meal.ArchiveURL.path!)
+    if !isSuccessfulSave {
+      print("Failed to save meals...")
+    }
+    printMeals() // Debug
+  }
+
+  func loadMeals() -> [Meal]? {
+    return NSKeyedUnarchiver.unarchiveObjectWithFile(Meal.ArchiveURL.path!) as? [Meal]
   }
 
 }
